4543fe65d78b32635da412ef346b1222cecd9450
Arpan Srivastava <iamarpan2411@gmail.com>
2025-03-02 20:48:50 +0530

    vishwamitra smriti

--- smRtiH/vishwamitra_smriti/scrapper.py
+++ smRtiH/vishwamitra_smriti/scrapper.py
@@ -1,6 +1,11 @@
 import re
 import json
 
+def english_to_hindi_numerals(number):
+    hindi_numerals = {"0": "०", "1": "१", "2": "२", "3": "३", "4": "४", "5": "५", "6": "६", "7": "७", "8": "८",
+                      "9": "९"}
+    return "".join(hindi_numerals[digit] for digit in number)
+
 def extract_verses_from_md(input_file, output_file="verses_output.json"):
     # Read the file
     with open(input_file, "r", encoding="utf-8") as file:
@@ -13,7 +18,7 @@ def extract_verses_from_md(input_file, output_file="verses_output.json"):
     verse_pattern = re.compile(r'^(.*)\s(\d+)$', re.DOTALL)
 
     verses = []
-
+    adhyay=0
     # Process each paragraph
     for paragraph in paragraphs:
         lines = paragraph.split("\n")  # Split into individual lines
@@ -30,11 +35,12 @@ def extract_verses_from_md(input_file, output_file="verses_output.json"):
         if match:
             verse_text, verse_number = match.groups()
             verse_text = " ".join(verse_text.split())  # Normalize spaces
-
+            if verse_number == '१':
+                adhyay+=1
             # Append to list
             verses.append({
                 "verse": verse_text,
-                "ref": f"vishwamitra_smriti->{verse_number}"
+                "ref": f"vishwamitra_smriti->{english_to_hindi_numerals(str(adhyay))}.{verse_number}"
             })
 
     # Write to JSON file
